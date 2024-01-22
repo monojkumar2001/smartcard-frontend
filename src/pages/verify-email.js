0ee138e32393e888de8efb3e3d27de9287979424
helaluddin100 <freelancerhelaluddin@gmail.com>
2024-01-23 01:43:33 +0600

    registration and email verification done

--- src/pages/verify-email.js
+++ src/pages/verify-email.js
@@ -1,5 +1,3 @@
-// pages/verify-email.js
-
 import { useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
@@ -9,44 +7,46 @@ const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const VerifyEmail = () => {
   const router = useRouter();
-  const { email } = router.query; // Get email from the query parameters
+  const { email } = router.query;
 
-  const [verificationCode, setVerificationCode] = useState("");
+  const [formData, setFormData] = useState({
+    verification_code: "",
+    email,
+  });
+
   const [message, setMessage] = useState("");
 
-  const handleVerification = async () => {
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleRegister = async () => {
     try {
-      if (email && verificationCode) {
-        const response = await axios.post(`${apiUrl}/api/auth/verify`, {
-          email,
-          code: verificationCode,
-        });
-        setMessage(response.data.message);
-      } else {
-        setMessage("Invalid verification code.");
+      const response = await axios.post(`${apiUrl}/api/auth/verify`, formData);
+      setMessage(response.data.message);
+      if (response.data.message.includes("Registration successful")) {
+        router.push(`/login`);
       }
     } catch (error) {
-      if (error.response.status === 422) {
-        setMessage("Invalid verification code. Please try again.");
-      } else {
-        setMessage("An unexpected error occurred. Please try again later.");
-      }
+      console.error("Registration failed", error);
+      setMessage("Registration failed. Please try again.");
     }
   };
 
   return (
-    <div>
-      <h1>Email Verification</h1>
-      <p>Email: {email}</p>
+    <div className="cmt-8">
+      <h1>Registration</h1>
+      {formData.email}
+      {formData.verification_code}
       <div>
         <label>Verification Code:</label>
+
         <input
           type="text"
-          value={verificationCode}
-          onChange={(e) => setVerificationCode(e.target.value)}
+          name="verification_code"
+          onChange={handleInputChange}
         />
       </div>
-      <button onClick={handleVerification}>Verify Email</button>
+      <button onClick={handleRegister}>Register</button>
       {message && <p>{message}</p>}
     </div>
   );
