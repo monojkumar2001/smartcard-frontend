0544e51d7616a5f60847bc26950d1791b5eeca02
monoj kumar <monoj.womenindigital@gmail.com>
2024-01-25 11:33:21 +0600

    dasboard app remove

--- src/components/Layout/DashboardApp.js
+++ src/components/Layout/DashboardApp.js
@@ -1,22 +1,22 @@
-import React from "react";
-import DashboardLayout from "../../component/Layout/DashboardLayout";
+// import React from "react";
+// import DashboardLayout from "../../component/Layout/DashboardLayout";
 
-const DashboardApp = ({ Component, pageProps }) => {
-  useEffect(() => {
-    Aos.init({
-      easing: "ease-out-cubic",
-      once: true,
-      offset: 50,
-    });
-  }, []);
+// const DashboardApp = ({ Component, pageProps }) => {
+//   useEffect(() => {
+//     Aos.init({
+//       easing: "ease-out-cubic",
+//       once: true,
+//       offset: 50,
+//     });
+//   }, []);
 
-  return (
-    <>
-      <DashboardLayout>
-        <Component {...pageProps} />
-      </DashboardLayout>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <DashboardLayout>
+//         <Component {...pageProps} />
+//       </DashboardLayout>
+//     </>
+//   );
+// };
 
-export default DashboardApp;
+// export default DashboardApp;
